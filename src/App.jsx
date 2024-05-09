537fed8bbd60294524bb983780642c0cee3fbedc
gabasaura <gabriela.garin@gmail.com>
2024-05-09 11:01:13 -0400

    update repositories

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
