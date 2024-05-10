576f6ee09c7a16fd2e1a13f32e2a87f940f58c7e
gabasaura <gabriela.garin@gmail.com>
2024-05-10 14:22:01 -0400

    contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const form = useRef();
 
     function handleFormSubmit(event) {
         event.preventDefault();
         if (!validateForm()) {
-            // Perform form submission logic here
-            console.log("Form submitted successfully!");
-            clearForm(); // Clear form fields
+            emailjs
+                .sendForm('service_skf16t8', 'template_28gw5tq', form.current, {
+                    publicKey: 'xy1w6sUa4TwUqWdWi',
+                })
+                .then(
+                    () => {
+                        console.log('SUCCESS!');
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
         }
+
+        console.log("Form submitted successfully!");
+        clearForm(); // Clear form fields
     }
+
 
     function handleInputChange(event) {
         const { id, value } = event.target;
@@ -55,6 +71,7 @@ const Contact = () => {
         return emailPattern.test(email);
     }
 
+
     function clearForm() {
         setName("");
         setEmail("");
@@ -65,22 +82,22 @@ const Contact = () => {
     return (
         <div className="py-md-5 align-items-center justify-content-center">
             <h1 className=" text-center">Contact Us</h1>
-            <form className="rounded-3" onSubmit={handleFormSubmit}>
+            <form ref={form} className="rounded-3" onSubmit={handleFormSubmit}>
                 <div className="mt-5 w-75 card mx-auto p-0">
                     <div className="card-body">
                         {errorMessage && <div className="alert alert-danger m-3" role="alert">{errorMessage}</div>}
                         <div className="row mx-2">
                             <div className="col-12 mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
-                                <input type="text" className="form-control" id="name" value={name} onChange={handleInputChange} />
+                                <input name="user_name" type="text" className="form-control" id="name" value={name} onChange={handleInputChange} />
                             </div>
                             <div className="col-12 mb-3">
                                 <label htmlFor="email" className="form-label">Email</label>
-                                <input type="email" className="form-control" id="email" value={email} onChange={handleInputChange} />
+                                <input name="user_email" type="email" className="form-control" id="email" value={email} onChange={handleInputChange} />
                             </div>
                             <div className="col-12 mb-3">
                                 <label htmlFor="message" className="form-label">Message</label>
-                                <textarea className="form-control" id="message" rows="5" maxLength="250" value={message} onChange={handleInputChange} />
+                                <textarea name="message" className="form-control" id="message" rows="5" maxLength="250" value={message} onChange={handleInputChange} />
                                 <small className="text-secondary">{250 - message.length} characters remaining</small>
                             </div>
                         </div>
@@ -94,7 +111,7 @@ const Contact = () => {
                 </div>
             </form>
         </div>
-    );
-};
+    )
+}
 
 export default Contact;
