25e6c674469f084c4afba8086e7eb07aef15b24a
Nicolás Guzmán <n3kguzman@gmail.com>
2024-05-16 20:38:55 -0400

    agregando renderizado condicional textarea modal

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Modal } from 'bootstrap';
-
+import '../styles/texto.css'
 
 const CreateAccount = () => {
   return (
-    <div>
-        <Nav.Link as={Link} to="/register" className={location.pathname === '/register' ? 'active' : ''}><h6> <u>Sign up for leaving a comment. </u></h6></Nav.Link>
+    <div className='d-flex justify-content-center my-3'>
+       <Nav.Link as={Link} to="/register" className={location.pathname === '/register' ? 'active' : ''}><h6 data-bs-dismiss="modal"> <u>Sign up</u></h6></Nav.Link> <h6 className='mt-2 mx-2 texto'> <u> or </u> </h6><Nav.Link as={Link} to="/login" className={location.pathname === '/register' ? 'active' : ''}><h6 data-bs-dismiss="modal"> <u>Log in </u></h6></Nav.Link> <h6 className='mt-2 mx-2 texto'> <u>to leave a comment</u></h6>
     </div>
   )
 }
