23c88077d6a47cb3446155d751ed222f5acba1d6
gabasaura <gabriela.garin@gmail.com>
2024-05-20 16:41:22 -0400

    pipolist, no pipolist, pipocard

--- src/components/NoPipoList.jsx
+++ src/components/NoPipoList.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const NoPipoList = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
-                <div className="flex-column flex-grow-1 w-75 mx-auto p-5 align-items-center justify-content-center">
-                    <h1 className="text-center">Hey! Awesome news!</h1>
-
-                    <h3 className="text-center my-4">No more Pipos are waiting in line! (つ✧ω✧)つ	</h3>
-    
+      <div className="flex-column flex-grow-1 w-75 mx-auto p-5 align-items-center justify-content-center">
+        <div className='border border-1 border-info p-3'>
+        <h1 className="text-center">Everythig is Ok!</h1>
+        <h3 className="text-center my-4">No more Pipos are waiting in line! (つ✧ω✧)つ	</h3>
+        </div>
       </div>
     </div>
   )
--- src/components/PipoCard.jsx
+++ src/components/PipoCard.jsx
@@ -9,19 +9,19 @@ const PipoCard = ({ name, address, stars, addPipo, deletePipo, id }) => {
 
     return (
         <>
-        <div className='row d-flex align-items-center justify-content-center'>
+        <div className='d-flex align-items-center justify-content-center'>
           <div className="w-75">
             <div className="row g-0">
-              <div className="col-11">
-                <div className="card-body">
-                  <h5 className="card-title">Name: {name}</h5>
-                  <p className="card-text">Address: {address}</p>
+              <div className="col-8 col-md-10">
+                <div className="card-body pt-3">
+                  <h5 className="card-title">Pipo Name: {name}</h5>
+                  <p className="card-text">Pipo Address: {address}</p>
                   <p className="card-text"><small className="text-body-secondary">{stars}</small></p>
                 </div>
               </div>
-              <div className='contenedor-icono d-flex justify-content-center col-1'>
-                <FaCheck onClick={() => addPipo(id)} className='btn btn-outline-info p-4 mx-1' />
-                <MdDelete onClick={() => deletePipo(id)} className='btn btn-outline-info p-4 mx-1' />
+              <div className='contenedor-icono d-flex justify-content-center col-4 col-md-2 pt-3'>
+                <FaCheck onClick={() => addPipo(id)} className='icono-pipocard btn btn-outline-info mx-1' />
+                <MdDelete onClick={() => deletePipo(id)} className='icono-pipocard btn btn-outline-info mx-1' />
               </div>
             </div>
           </div>
